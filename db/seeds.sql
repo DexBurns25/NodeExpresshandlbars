4304706e0b5a639e123ee5db753345f2659bc390
DexBurns25 <71415601+DexBurns25@users.noreply.github.com>
2021-01-07 18:56:38 -0500

    adding DB schema files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Beyond Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Luther Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Impossible Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('In-N-Out Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
