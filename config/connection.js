1e5cd7d6d99faecfd2c3a1d4a960444f74092053
Dexter D Burns <71415601+DexBurns25@users.noreply.github.com>
2021-01-14 18:21:24 -0500

    updating files

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,14 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "localhost",
   port: 3306,
-  user: "i7kq82s5sfdqvqeo",
-  password: "yfm68vhalr75wppr",
-  database: "gabxd02gy6sa9dbs"
+  user: "root",
+  password: "Mia46louise",
+  database: "burgers_db"
 })
 };
 
-
-
-//
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -25,5 +22,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-//
 module.exports = connection;
